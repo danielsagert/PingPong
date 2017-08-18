4b61dc5e832229c1f81abd4f76ee61c8e834071a
Daniel Sagert <daniel.sagert@gmail.com>
2017-08-18 21:27:47 +0200

    Optimized Docker build and run scripts

--- build.js
+++ build.js
@@ -1,4 +1,2 @@
 #!/usr/bin/jjs -fv
-var cmd = "docker build -t danielsagert/pingpong ."
-var System = Java.type("java.lang.System");
-$EXEC(cmd, System.in, System.out, System.err);
+$EXEC("docker build -t danielsagert/pingpong .");
